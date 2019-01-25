acc34df21edc99abb9f179f66810b6b4bffd7e1a
johan.park <johan.park@kakaocorp.com>
2019-01-25 17:54:09 +0900

    Change color of Atom theme

--- themes/Atom.js
+++ themes/Atom.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#000000", "#fd5ff1", "#87c38a", "#ffd7b1", "#85befd", "#b9b6fc", "#85befd", "#e0e0e0", "#000000", "#fd5ff1", "#94fa36", "#f5ffa8", "#96cbfe", "#b9b6fc", "#85befd", "#e0e0e0"]);
-t.prefs_.set('foreground-color', "#c5c8c6");
-t.prefs_.set('background-color', "#161719");
-t.prefs_.set('cursor-color', 'rgba(208,208,208,0.5)');
+t.prefs_.set('color-palette-overrides',["#343a43", "#e26a72", "#96c475", "#d29a61", "#5cadf1", "#c774df", "#51b5c3", "#abb1bf", "#343a43", "#e26a72", "#96c475", "#d29a61", "#5cadf1", "#c774df", "#51b5c3", "#abb1bf"]);
+t.prefs_.set('foreground-color', "#abb1bf");
+t.prefs_.set('background-color', "#272c33");
+t.prefs_.set('cursor-color', 'rgba(79,135,255,0.5)');
